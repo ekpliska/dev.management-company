fc424b048c10992fa97f810089f474d97e3196a3
ekpliska <pliska.ek@gmail.com>
2018-11-29 15:08:05 +0300

    fixed css on the page of vote

--- modules/clients/views/voting/index.php
+++ modules/clients/views/voting/index.php
@@ -8,112 +8,35 @@
 $this->title ="Голосование"
 ?>
 
-<div class="row vote-lists">
-<?php if (isset($voting_list) && count($voting_list) > 0) : ?>
-    <?php foreach ($voting_list as $key => $voting) : ?>
-
-    <div class="col-md-4">
-        <div class="vote-card-preview">
-            <?= Html::img('@web' . $voting['voting_image'], ['class' => 'vote-card-img']) ?>
-            <span class="badge vote-card_date-end">
-                <?= FormatHelpers::numberOfDaysToFinishVote($voting['voting_date_start'], $voting['voting_date_end']) ?>
-            </span>
-            <div class="vote-card_title">
-                <a href="<?= Url::to(['voting/view-voting', 'voting_id' => $voting['voting_id']]) ?>">
-                    <?= FormatHelpers::shortTitleOrText($voting['voting_title'], 25) ?>
-                </a>
-                <i class="glyphicon glyphicon-ok"></i>
-                <!--<i class="glyphicon glyphicon-flag"></i>-->
-            </div>
-            <div class="vote-card_text">
-                <?= FormatHelpers::shortTitleOrText($voting['voting_text'], 250) ?>
-            </div>
-            <div class="vote-card_count">
-                <div class="col-md-2">Проголосовало</div>
-                <div class="col-md-10 text-right"><span class="vote-bange-count">#TODO</span></div>
-            </div>
-            <div class="vote-card_participants">
-                <div class="col-md-2">Участвуют</div>
-                <div class="col-md-10 text-right"><span class="vote-bange-count">#TODO</span></div>
-            </div>
-        </div>
-    </div>
-<?php /*    
-<div class="card requests-card-preview  box-shadow" data-toggle="modal" data-target=".bd-example-modal-lg">
-    <div class="requests-card-preview-container">
-        <?= Html::img('@web' . $voting['voting_image'], ['class' => 'card-img-top requests-card-img-top']) ?>
-        <span class="badge req-img-badge">
-            <?= FormatHelpers::numberOfDaysToFinishVote($voting['voting_date_start'], $voting['voting_date_end']) ?>
-        </span>
-    </div>
-    <div class="cont-ref">
-        <h5 class="requests-card-preview-h row">
-            <a href="<?= Url::to(['voting/view-voting', 'voting_id' => $voting['voting_id']]) ?>">
-                <?= FormatHelpers::shortTitleOrText($voting['voting_title'], 25) ?>
-            </a>
-        </h5>
-        <div class="<?= $voting['status'] == Voting::STATUS_CLOSED ? 'requests-icon-container-green ml-auto' : 'requests-icon-container-blue ml-auto' ?>">
-            <?= Html::img('/images/clients/' . ($voting['status'] == Voting::STATUS_CLOSED ? 'check.svg' : 'flag.svg')) ?>
-        </div>
-    </div>
-    <div class="card-body m-0 p-0 requests-card-preview-body">
-        <p class="card-text requests-card-text">
-            <?= FormatHelpers::shortTitleOrText($voting['voting_text'], 250) ?>
-        </p>
-    </div>
-    <div class="requests-card-footer">
-        <div class="requests-card-footer-txt-block">
-            <p class="requests-card-footer-txt">Проголосовало<img class="micro-circle rounded-circle requests-face-micro" src="assets/img/noname.jpg"><img class="micro-circle rounded-circle" src="assets/img/noname.jpg"><img class="micro-circle rounded-circle" src="assets/img/noname.jpg"><img class="micro-circle rounded-circle" src="assets/img/noname.jpg">
-                <span class="badge badge-darkblue requests-badge">18</span>
-            </p>
-        </div>
-        <div class="requests-card-footer-txt-block">
-            <p class="requests-card-footer-txt">Участвуют<img class="micro-circle rounded-circle requests-face-micro-member" src="assets/img/noname.jpg"><img class="micro-circle rounded-circle" src="assets/img/noname.jpg"><img class="micro-circle rounded-circle" src="assets/img/noname.jpg"><img class="micro-circle rounded-circle" src="assets/img/noname.jpg">
-                <span class="badge badge-darkblue requests-badge">78</span>
-            </p>
-        </div>
-    </div>
-</div>
-*/ ?>
-    <?php endforeach; ?>
-<?php endif; ?>
-</div>
-<?php /*
-<div class="clients-default-index">
-    
-    <h1><?= $this->title ?></h1>
-    
+<div class="vote-lists row">
     <?php if (isset($voting_list) && count($voting_list) > 0) : ?>
         <?php foreach ($voting_list as $key => $voting) : ?>
 
-            <div class="col-md-4">
-                <a href="<?= Url::to(['voting/view-voting', 'voting_id' => $voting['voting_id']]) ?>">
-                    <?= Html::img('@web' . $voting['voting_image'], ['alt' => $voting['voting_title'], 'style' => 'width:100%']) ?>
-                </a>
-                
-                <h5>
-                    <?= FormatHelpers::statusNameVoting($voting['status']) ?>
-                </h5>
-                
-                <h4>
-                    <?= Html::a($voting['voting_title'], ['view-voting', 'voting_id' => $voting['voting_id']]) ?>
-                </h4>
-                
-                <?= FormatHelpers::formatDate($voting['voting_date_start'], false) ?>
-                <br />
-                <?= FormatHelpers::numberOfDaysToFinishVote($voting['voting_date_start'], $voting['voting_date_end']) ?>
-                <br />
-                <p><?= FormatHelpers::shortTextNews($voting['voting_text']) ?></p>
-                
-                <?= Html::a('Голосовать', ['voting/view-voting', 'voting_id' => $voting['voting_id']]) ?>
-                    
+        <div class="col-md-4">
+            <div class="vote-card-preview">
+                <?= Html::img('@web' . $voting['voting_image'], ['class' => 'vote-card-img']) ?>
+                <span class="vote-card_date-end">
+                    <?= FormatHelpers::numberOfDaysToFinishVote($voting['voting_date_start'], $voting['voting_date_end']) ?>
+                </span>
+                <div class="vote-card_title">
+                    <a href="<?= Url::to(['voting/view-voting', 'voting_id' => $voting['voting_id']]) ?>">
+                        <?= FormatHelpers::shortTitleOrText($voting['voting_title'], 25) ?>
+                    </a>
+                    <i class="glyphicon glyphicon-<?= $voting['status'] == Voting::STATUS_CLOSED ? 'flag' : 'ok' ?>"></i>
+                </div>
+                <div class="vote-card_text">
+                    <?= FormatHelpers::shortTitleOrText($voting['voting_text'], 250) ?>
+                </div>
+                <div class="vote-card_count">
+                    <div class="col-md-2">Проголосовало</div>
+                    <div class="col-md-10 text-right"><span class="vote-bange-count">#TODO</span></div>
+                </div>
+                <div class="vote-card_participants">
+                    <div class="col-md-2">Участвуют</div>
+                    <div class="col-md-10 text-right"><span class="vote-bange-count">#TODO</span></div>
+                </div>
             </div>
-
-            <?php if (($key + 1) % 3 == 0) : ?>
-                <div class="clearfix"></div>
-            <?php endif; ?>
-
+        </div>
         <?php endforeach; ?>
     <?php endif; ?>
-        
 </div>
