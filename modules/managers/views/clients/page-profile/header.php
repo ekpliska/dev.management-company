12b2534a8609eef567f67555ad858cba9ba4ccaf
ekpliska <pliska.ek@gmail.com>
2018-12-12 16:24:42 +0300

    fixed btn

--- modules/managers/views/clients/page-profile/header.php
+++ modules/managers/views/clients/page-profile/header.php
@@ -18,18 +18,11 @@
     <div class="col-md-8 col-xs-6 text-left user-info-block">
         <p class="user-info-block_name">
             <?= $client_info->fullName ?>
+            <?php if ($account_choosing->account_balance < 0) : ?>
+                <span class="debt-client">&ndash;&#8381;</span>
+            <?php endif; ?>                
         </p>
 
-  
-
-        <?= Html::dropDownList('clients-account_list', $account_choosing->account_id, $list_account, [
-                'placeHolder' => $account_choosing->account_number,
-                'id' => 'select-dark',
-                'class' => 'custom-select-dark',
-                'data-client' => $client_info->clients_id,
-                'data-url' => Yii::$app->controller->action->id,
-        ]) ?>
-        
         <div class="control-block">
             <?php if ($user_info->status == 1) : ?>
                 <?= Html::button('Заблокировать', [
@@ -49,6 +42,16 @@
                         'data-user' => $user_info->user_id]) 
             ?>
         </div>
+  
+
+        <?= Html::dropDownList('clients-account_list', $account_choosing->account_id, $list_account, [
+                'placeHolder' => $account_choosing->account_number,
+                'id' => 'select-dark',
+                'class' => 'custom-select-dark',
+                'data-client' => $client_info->clients_id,
+                'data-url' => Yii::$app->controller->action->id,
+        ]) ?>
+        
     </div>
 </div>
 
