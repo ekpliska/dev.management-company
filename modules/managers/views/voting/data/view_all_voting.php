01e3b860de362ae0a5b6b01fff6f05c414033e0a
ekpliska <pliska.ek@gmail.com>
2018-12-29 10:53:45 +0300

    fixed general page of vote

--- modules/managers/views/voting/data/view_all_voting.php
+++ modules/managers/views/voting/data/view_all_voting.php
@@ -18,22 +18,45 @@
     </div>
     <div class="vote-card__content">
         <h1 class="vote-card__content_title">
-            <?= Html::a($voting['voting_title'], ['voting/view', 'voting' => $voting['voting_id']]) ?>
-            <span>ID <?= $voting['voting_id'] ?></span>
+            <?= Html::a($voting['voting_title'] . " <span>ID{$voting['voting_id']}</span>", ['voting/view', 'voting' => $voting['voting_id']]) ?>
+            <?= Html::button('<i class="glyphicon glyphicon-trash"></i>', [
+                    'class' => 'btn btn-sm pull-right btn-delete-question',
+                    'data-toggle' => 'modal',
+                    'data-target' => '#delete_voting_manager',
+                    'data-voting' => $voting['voting_id']]) ?>
         </h1>
+        <div class="col-md-12 vote-card__content_author">
+            <span><i class="glyphicon glyphicon-user"></i> <?= FormatFullNameUser::nameEmployerByUserID($voting['voting_user_id']) ?></span>
+        </div>
         <div class="vote-card__content_info">
-            <div class="col-md-2">1</div>
-            <div class="col-md-4">1</div>
-            <div class="col-md-4">1</div>
-            <div class="col-md-2">1</div>
-            <span class="label label-success"><?= FormatHelpers::statusNameVoting($voting['status']) ?></span>
-            <span class="vote_status">1</span>
-            <span class="vote_status">1</span>
-            <span class="vote_status">1</span>
+            <div class="col-md-1 col-xs-6">
+                <span class="status-vote-<?= $voting['status'] ?>"><?= FormatHelpers::statusNameVoting($voting['status']) ?>
+            </div>
+            <div class="col-md-4 col-xs-6">
+                <span class="title">Начало голосования: </span>
+                <?= FormatHelpers::formatDate($voting['voting_date_start'], true, 1, false) ?>
+            </div>
+            <div class="col-md-4 col-xs-6">
+                <span class="title">Завершение голосование: </span>
+                <?= FormatHelpers::formatDate($voting['voting_date_end'], true, 1, false) ?>
+            </div>
+            <div class="col-md-3 col-xs-6">
+                <span class="title">Проголосовало: </span>
+                #TODO
+            </div>
+        </div>
+        <div class="col-md-12 vote-card__content_text">
+            <?= HtmlPurifier::process(strip_tags($voting['voting_text'])) ?>
         </div>
     </div>
 </div>
 <?php endforeach; ?>
+
+<?= LinkPager::widget([
+        'pagination' => $pages
+    ]);
+?>
+
 <?php /*
 <?php foreach ($view_all_voting as $voting) : ?>
     <div class="col-md-12">
@@ -57,7 +80,7 @@
             </p>
             <p>
                 <span class="label label-success"><?= FormatHelpers::statusNameVoting($voting['status']) ?></span>
-                <b>Дата начала голосования:</b> <?= FormatHelpers::formatDate($voting['voting_date_start'], true, 1) ?>
+                <b>Дата начала голосования:</b> 
                 <b>Дата окончания голосования:</b> <?= FormatHelpers::formatDate($voting['voting_date_end'], true, 1) ?>
                 <b>Проголосовало:</b> #COUNT
             </p>
