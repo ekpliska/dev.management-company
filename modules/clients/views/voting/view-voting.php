30d356b478b5fb3c614dabbd605bf9cb045cfbea
ekpliska <pliska.ek@gmail.com>
2019-01-12 21:03:30 +0300

    fixed

--- modules/clients/views/voting/view-voting.php
+++ modules/clients/views/voting/view-voting.php
@@ -7,6 +7,7 @@
     use app\models\Voting;
     use app\models\Answers;
     use app\modules\clients\widgets\AlertsShow;
+    use app\models\RegistrationInVoting;
 
 /* 
  * Просмотр отдельного голосования
@@ -14,10 +15,6 @@
 $this->title = Yii::$app->params['site-name'] . $voting['voting_title'];
 $this->params['breadcrumbs'][] = ['label' => 'Опрос', 'url' => ['voting/index']];
 $this->params['breadcrumbs'][] = $voting['voting_title'];
-
-$_start = strtotime($voting['voting_date_start']);
-$_end = strtotime($voting['voting_date_end']);
-$_now = time();
 ?>
 
 <?= Breadcrumbs::widget([
@@ -39,7 +36,17 @@ $_now = time();
             <p class="voting-description">
                 <?= $voting['voting_text'] ?>
             </p>
-            <?php if (($voting['status'] == Voting::STATUS_ACTIVE || Yii::$app->user->can('clients')) && $is_register == false) : ?>
+            <?php 
+                /* 
+                 * "Принять участие" достпуно только в активных голосованиях, 
+                 * Собственнику,
+                 * Участнику со статусом "на регистрации"
+                 */
+                if (
+                        ($voting['status'] == Voting::STATUS_ACTIVE || Yii::$app->user->can('clients')) 
+                        && $is_register['status'] == RegistrationInVoting::STATUS_DISABLED
+                    ) : 
+            ?>
                 <?= Html::button('Принять участие', [
                         'class' => 'register-in-voting',
                         'id' => 'get-voting-in',
@@ -54,72 +61,79 @@ $_now = time();
             <p>
                 <span class="span-count"><?= count($participants) ?></span> Проголосовало
             </p>
-            <?php if (isset($participants) && count($participants) > 0) : ?>
-            <?php foreach ($participants as $participant) : ?>
-                <div class="col-md-4 voting__participant_info text-center">
-                    <?php $avatar = $participant['user_photo'] ? $participant['user_photo'] : "images/no-avatar.jpg" ?>
-                    <?= Html::img("@web/{$avatar}", ['alt' => 'user-name', 'class' => 'img-responsive img-circle']) ?>
-                    <?= Html::a ($participant['clients_name'], ['/']) ?>
-                </div>
-            <?php endforeach; ?>
+            <?php
+                /*
+                 * Блок формирования участников, завершивших голосование
+                 */
+                if (isset($participants) && count($participants) > 0) : 
+            ?>
+                <?php foreach ($participants as $participant) : ?>
+                    <div class="col-md-4 voting__participant_info text-center">
+                        <?php $avatar = $participant['user_photo'] ? $participant['user_photo'] : "images/no-avatar.jpg" ?>
+                        <?= Html::img("@web/{$avatar}", ['alt' => 'user-name', 'class' => 'img-responsive img-circle']) ?>
+                        <?= Html::a ($participant['clients_name'], ['/']) ?>
+                    </div>
+                <?php endforeach; ?>
             <?php endif; ?>
         </div>
         
         <div class="col-md-9 voting-body_right">
-            <?php if ($is_register == false) : ?>
-            <?php foreach ($voting['question'] as $key => $question) : ?>
-                <div class="questions-text">
-                    <span><?= $question['questions_text'] ?></span>
-                    <span class="span-count"><?= count($question['answer']) ?></span> Проголосовало
-                </div>
-            <?php endforeach; ?>
+            <?php /* Контент для участников голосования не подтвердивших свое участие */ ?>
+            <?php if ($is_register['status'] == RegistrationInVoting::STATUS_DISABLED) : ?>
+                <?php foreach ($voting['question'] as $key => $question) : ?>
+                    <div class="questions-text">
+                        <span><?= $question['questions_text'] ?></span>
+                        <span class="span-count"><?= count($question['answer']) ?></span> Проголосовало
+                    </div>
+                <?php endforeach; ?>
             
             <?php else : ?>
-            <?php /* Контент для зарегистрировавщихся участников */ ?>
+                <?php /* Контент для зарегистрировавщихся участников */ ?>
             
-            <div class="questions-text-show-form">
-                <?php $count_answer = 0; ?>
-                <?php foreach ($voting['question'] as $key => $question) : ?>
-                <div class="questions-text-show">
-                    <h4>
-                        <?php 
-                            // Ответ
-                            $_answer = $question['answer'][0]['answers_vote'];
-                        ?>
-                        <i class="glyphicon glyphicon-ok marker-vote-<?= $question['questions_id'] ?> <?= !isset($_answer) ? 'show-marker' : '' ?>"></i>
-                        <?= $question['questions_text'] ?>
-                    </h4>
-                    <div class="answers-block">
-                        #TODO %
-                    </div>
-                    <div class="btn-block text-center">
-                        <div class="btn-group btn-group-lg" role="group" aria-label="Button block" id="btn-group-<?= $key ?>">
-                            <?php foreach (Answers::getAnswersArray() as $type_answer => $answer) : ?>
-                                <?php $class = ($_answer == $type_answer) ? 'btn-set-voting-active' : ''; ?>
-                                <?= Html::button($answer, [
-                                    'class' => "btn btn-primary btn-set-voting {$class}",
-                                    'id' => "btn-vote-yes-{$key}",
-                                    'data-question' => $question['questions_id'],
-                                    'data-type-answer' => $type_answer,
-                                ]) ?>
-                            <?php endforeach; ?>
+                <div class="questions-text-show-form">
+                    <?php $count_answer = 0; ?>
+                    <?php foreach ($voting['question'] as $key => $question) : ?>
+                    <div class="questions-text-show">
+                        <h4>
+                            <?php 
+                                // Ответ
+                                $_answer = $question['answer'][0]['answers_vote'];
+                            ?>
+                            <i class="glyphicon glyphicon-ok marker-vote-<?= $question['questions_id'] ?> <?= !isset($_answer) ? 'show-marker' : '' ?>"></i>
+                            <?= $question['questions_text'] ?>
+                        </h4>
+                        <div class="answers-block">
+                            #TODO %
                         </div>
+                        <?php if ($is_register['finished'] == RegistrationInVoting::STATUS_FINISH_NO) : ?>
+                            <div class="btn-block text-center">
+                                <div class="btn-group btn-group-lg" role="group" aria-label="Button block" id="btn-group-<?= $key ?>">
+                                    <?php foreach (Answers::getAnswersArray() as $type_answer => $answer) : ?>
+                                        <?php $class = ($_answer == $type_answer) ? 'btn-set-voting-active' : ''; ?>
+                                        <?= Html::button($answer, [
+                                            'class' => "btn btn-primary btn-set-voting {$class}",
+                                            'id' => "btn-vote-yes-{$key}",
+                                            'data-question' => $question['questions_id'],
+                                            'data-type-answer' => $type_answer,
+                                        ]) ?>
+                                    <?php endforeach; ?>
+                                </div>
+                            </div>
+                        <?php endif; ?>
                     </div>
+                    <?php endforeach; ?>
                 </div>
-                <?php endforeach; ?>
-            </div>
-            
-            <?php if (count($voting['question']) != count($voting['question']['answer'])) : ?>
-            <?php echo '<pre>'; var_dump($count_answer) ?>
-                <div class="finished-voting text-center">
-                    <?= Html::button('Проголосовать', [
-                            'id' => 'finished-voting-btn',
-                            'class' => 'blue-btn',
-                            'data-voting' => $voting['voting_id'],
-                            'data-question' => count($voting['question']),
-                    ]) ?>
-                </div>
-            <?php endif; ?>
+                
+                <?php if ($is_register['finished'] == RegistrationInVoting::STATUS_FINISH_NO) : ?>
+                    <div class="finished-voting text-center">
+                        <?= Html::button('Проголосовать', [
+                                'id' => 'finished-voting-btn',
+                                'class' => 'blue-btn',
+                                'data-voting' => $voting['voting_id'],
+                                'data-question' => count($voting['question']),
+                        ]) ?>
+                    </div>
+                <?php endif; ?>
             
             <?php endif; ?>
             
@@ -146,6 +160,13 @@ $_now = time();
 ?>
 
 <?php
+// Дата начала голосования
+$_start = strtotime($voting['voting_date_start']);
+// Дата завершения голосования
+$_end = strtotime($voting['voting_date_end']);
+// Декущая дата
+$_now = time();
+
 $this->registerJs("
     
     function checkDate(){
