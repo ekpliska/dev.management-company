d0af9e5ec1688664d90e4fec9c004c1db4f37e4b
ekpliska <pliska.ek@gmail.com>
2019-01-17 16:07:53 +0300

    changed query

--- modules/managers/models/PaidServices.php
+++ modules/managers/models/PaidServices.php
@@ -22,21 +22,21 @@ class PaidServices extends BasePaidServices {
                         . 'ps.status as status, '
                         . 'cs.category_name as category, '
                         . 's.services_name as service_name, '
-                        . 'ed.employers_surname as surname_d, ed.employers_name as name_d, ed.employers_second_name as sname_d, '
-                        . 'es.employers_surname as surname_s, es.employers_name as name_s, es.employers_second_name as sname_s, '
-                        . 'he.estate_town as town, '
-                        . 'h.houses_street as street, h.houses_number_house as house, '
+                        . 'ed.employee_surname as surname_d, ed.employee_name as name_d, ed.employee_second_name as sname_d, '
+                        . 'es.employee_surname as surname_s, es.employee_name as name_s, es.employee_second_name as sname_s, '
+                        . 'c.clients_surname as clients_surname, c.clients_second_name as clients_second_name, c.clients_name as clients_name, '
+                        . 'h.houses_gis_adress as gis_adress, h.houses_number as house, '
                         . 'f.flats_porch as porch, f.flats_floor as floor, '
                         . 'f.flats_number as flat')
                 ->from('paid_services as ps')
-                ->join('LEFT JOIN', 'category_services as cs', 'cs.category_id = ps.services_category_services_id')
                 ->join('LEFT JOIN', 'services as s', 's.services_id = ps.services_name_services_id')
-                ->join('LEFT JOIN', 'employers as ed', 'ed.employers_id = ps.services_dispatcher_id')
-                ->join('LEFT JOIN', 'employers as es', 'es.employers_id = ps.services_specialist_id')
+                ->join('LEFT JOIN', 'category_services as cs', 'cs.category_id = s.services_category_id')
+                ->join('LEFT JOIN', 'employees as ed', 'ed.employee_id = ps.services_dispatcher_id')
+                ->join('LEFT JOIN', 'employees as es', 'es.employee_id = ps.services_specialist_id')
                 ->join('LEFT JOIN', 'personal_account as pa', 'pa.account_id = ps.services_account_id')
-                ->join('LEFT JOIN', 'flats as f', 'f.flats_id = pa.personal_house_id')
+                ->join('LEFT JOIN', 'flats as f', 'f.flats_id = pa.personal_flat_id')
                 ->join('LEFT JOIN', 'houses as h', 'h.houses_id = f.flats_house_id')
-                ->join('LEFT JOIN', 'housing_estates as he', 'he.estate_id = h.houses_estate_name_id') 
+                ->join('LEFT JOIN', 'clients as c', 'c.clients_id = pa.personal_clients_id')
                 ->orderBy(['ps.created_at' => SORT_DESC]);
         
         return $requests;
