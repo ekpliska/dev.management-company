aab2863cdcb03e47859496ac88ac2bb297527592
ekpliska <pliska.ek@gmail.com>
2019-01-23 15:47:14 +0300

    fiexd block of characteristics

--- modules/managers/views/housing-stock/_form/add_characteristic.php
+++ modules/managers/views/housing-stock/_form/add_characteristic.php
@@ -12,19 +12,29 @@
         'id' => 'add-characteristic',
         'enableAjaxValidation' => true,
         'validationUrl' => ['edit-description-validate', 'form' => 'add-characteristic'],
+        'validateOnChange' => false,
+        'validateOnBlur' => false,
+        'fieldConfig' => [
+            'template' => '<div class="field-modal">{label}{input}{error}</div>',
+        ],
     ]);
 ?>
+    <div class="hidden">
+        <?= $form->field($model, 'characteristics_house_id')->hiddenInput(['value' => $house_id, 'class' => 'hidden'])->label(false) ?>
+    </div>    
 
-    <?= $form->field($model, 'characteristics_house_id')->hiddenInput([
-            'value' => $house_id,
-            'class' => 'hidden',])
-        ->label(false) ?>
+    <?= $form->field($model, 'characteristics_name')
+            ->input('text', ['class' => 'field-input-modal'])
+            ->label($model->getAttributeLabel('characteristics_name'), ['class' => 'field-label-modal']) ?>
 
-    <?= $form->field($model, 'characteristics_name')->input('text')->label() ?>
+    <?= $form->field($model, 'characteristics_value')
+            ->input('text', ['class' => 'field-input-modal'])
+            ->label($model->getAttributeLabel('characteristics_value'), ['class' => 'field-label-modal']) ?>
 
-    <?= $form->field($model, 'characteristics_value')->input('text')->label() ?>
-
-    <?= Html::button('Отмена', ['class' => 'btn btn-default', 'data-dismiss' => 'modal']) ?>
-    <?= Html::submitButton('Добавить', ['class' => 'btn btn-primary']) ?>
+    
+    <div class="modal-footer">
+        <?= Html::submitButton('Добавить', ['class' => 'btn btn-modal-window btn-modal-window-yes']) ?>
+        <?= Html::button('Отмена', ['class' => 'btn btn-modal-window btn-modal-window-no', 'data-dismiss' => 'modal']) ?>
+    </div>
 
 <?php ActiveForm::end() ?>
