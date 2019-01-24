73f89172e407be24cd2242772c38c7ddb015e772
ekpliska <pliska.ek@gmail.com>
2019-01-24 14:21:31 +0300

    created the section of designer

--- modules/managers/views/designer-requests/data/index-paid-services.php
+++ modules/managers/views/designer-requests/data/index-paid-services.php
@@ -1,21 +1,75 @@
 <?php
 
     use yii\helpers\Url;
+    use yii\helpers\Html;
 
 /* 
  * Конструктор заявок, раздел Платные услуги
  */
 ?>
-<div id="_list-res" class="row housing-stock">
+<div class="row designer-block">
     <div class="col-md-5">
         <h4 class="title">Категория</h4>
+        <div class="designer-block__search-block">
+            <?= Html::input('text', 'search-services', null, ['class' => 'search-block__input', 'placeholder' => 'Поиск']) ?>
+        </div>
+        <div class="designer-block__lists">
+            <ul id="categories-list">
+                <li>
+                    Adele
+                    <span class="close category__delete" data-note="26"><i class="glyphicon glyphicon-trash"></i></span>
+                </li>
+                <li>
+                    Adele
+                    <span class="close category__delete" data-note="26"><i class="glyphicon glyphicon-trash"></i></span>
+                </li>
+                <li>
+                    Adele
+                    <span class="close category__delete" data-note="26"><i class="glyphicon glyphicon-trash"></i></span>
+                </li>
+                <li>
+                    Adele
+                    <span class="close category__delete" data-note="26"><i class="glyphicon glyphicon-trash"></i></span>
+                </li>
+            </ul>
+        </div>
+        
     </div>
         
     <div class="col-md-7">
         <h4 class="title">Услуга</h4>
+        <div class="designer-block__lists">
+            <ul class="services-list">
+                <li>
+                    Adele <span class="span-count">#TODO</span>
+                    <div class="dropdown dropdown__settings-service">
+                        <button type="button" class="btn-settings dropdown-toggle" data-toggle="dropdown">
+                            <i class="glyphicon glyphicon-option-horizontal"></i>
+                        </button>
+                        <ul class="dropdown-menu dropdown-setting">
+                            <li>Редактировать</li>
+                            <li>Удалить услугу</li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    Adele <span class="span-count">#TODO</span>
+                    <div class="dropdown dropdown__settings-service">
+                        <button type="button" class="btn-settings dropdown-toggle" data-toggle="dropdown">
+                            <i class="glyphicon glyphicon-option-horizontal"></i>
+                        </button>
+                        <ul class="dropdown-menu dropdown-setting">
+                            <li>Редактировать</li>
+                            <li>Удалить услугу</li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
         
 </div>
+
 <div class="dropup action-housing-stock">
     <button class="action-housing-stock__button dropdown-toggle" type="button" data-toggle="dropdown"></button>
     <ul class="dropdown-menu">
