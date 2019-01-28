110ad34cf8747de1fbb65f9b785e8aa8166df48d
ekpliska <pliska.ek@gmail.com>
2019-01-28 14:30:30 +0300

    fixed the page of one news

--- modules/clients/views/news/view-news.php
+++ modules/clients/views/news/view-news.php
@@ -18,31 +18,35 @@ $this->params['breadcrumbs'][] = $news['news_title'];
         'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
 ]) ?>
 
+<?php // var_dump($news); die(); ?>
 <div class="new-conteiner">
     <div class="preview-news">
     <?= FormatHelpers::previewNewsOrVote($news['news_preview'], true) ?>
     <div class="news-rubric-block">
-        <span class="rubric-name"><?= $news['news_type_rubric_id'] ?></span>
+        <span class="rubric-name"><?= $news['rubric']['rubrics_name'] ?></span>
             <?php if ($news['isAdvert'] == 1) : ?>
-                <span class="partner-name"><?= $news['partners_name'] ?></span>
+                <span class="partner-name"><?= $news['partner']['partners_name'] ?></span>
             <?php endif; ?>
     </div>
     <div class="news-title-block">
         <h2 class="news-title"><?= $news['news_title'] ?></h2>
         <p class="news-date"><?= FormatHelpers::formatDate($news['created_at'], false) ?></p>
-            
+    </div>
+    </div>
+
+    <div class="news-text">
+        <?= $news['news_text'] ?>
+    </div>
+    
+    <div class="load-news-documents">
+        <h4><span>Вложения</span></h4>
         <?php if (isset($files) && count($files)) : ?>
             <?php foreach ($files as $file) : ?>
                 <p class="load-documents">
                     <?= FormatHelpers::formatUrlByDoc($file['name'], $file['filePath']) ?>
                 </p>
             <?php endforeach; ?>
-        <?php endif; ?>
-    </div>
-    </div>
-
-    <div class="news-text">
-        <?= $news['news_text'] ?>
+        <?php endif; ?>        
     </div>
     
 </div>
