f63399f71ee41fd0c13d7985f16687b72e0521dc
ekpliska <pliska.ek@gmail.com>
2019-02-04 17:58:52 +0300

    fixed

--- modules/managers/views/requests/modal/show-grade-modal.php
+++ modules/managers/views/requests/modal/show-grade-modal.php
@@ -9,17 +9,16 @@
 ?>
 
 <?php if (isset($grade_info) && !empty($grade_info)) : ?>
-
-<?php foreach ($grade_info as $key => $grade_info) : ?>
 <table class="table grade-table">
     <tbody>
+        <?php foreach ($grade_info as $key => $grade_info) : ?>
         <tr>
             <td><?= $grade_info['question_text'] ?></td>
             <td><?= RequestAnswers::getAnswerText($grade_info['answers']['answer_value']) ?></td>
         </tr>
+        <?php endforeach; ?>
     </tbody>
 </table>
-<?php endforeach; ?>
 
 <div class="modal-footer">
     <?= Html::button('Закрыть', ['class' => 'btn-modal btn-modal-no', 'data-dismiss' => 'modal']) ?>
