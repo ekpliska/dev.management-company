21df5390db53bf8a89f35ba873d1953aa12d821e
ekpliska <pliska.ek@gmail.com>
2019-02-18 16:07:03 +0300

    fixed: to get cookies on page of housing stock

--- modules/dispatchers/views/housing-stock/index.php
+++ modules/dispatchers/views/housing-stock/index.php
@@ -9,7 +9,14 @@
  */
 $this->title = 'Жилищный фонд';
 $this->params['breadcrumbs'][] = 'Жилищный фонд';
-$active_item = $house_cookie ? $house_cookie : 0;
+
+/*
+ * Устанавливаем значение для активного дома, выбранного их списка
+ * @param array $house_cookie Массив приходит из куки
+ */
+$active_item = $house_cookie 
+        ? ['key' => $house_cookie['key'], 'value' => $house_cookie['value']] 
+        : ['key' => 0, 'value' => $houses_list[0]['houses_id']];
 ?>
 
 <div class="dispatcher-main">
@@ -36,7 +43,7 @@ $active_item = $house_cookie ? $house_cookie : 0;
                                 <?= FormatHelpers::formatHousingStosk($house['houses_gis_adress'], $house['houses_number']) ?>
                             </h4>
                         </div>
-                        <div id="house<?= $house['houses_id'] ?>" class="panel-collapse collapse house_accordion <?= $house_cookie['value'] == $house['houses_id'] ? 'in' : '' ?>">
+                        <div id="house<?= $house['houses_id'] ?>" class="panel-collapse collapse house_accordion <?= $active_item['value'] == $house['houses_id'] ? 'in' : '' ?>">
                             <div class="panel-body house-description">
                                 <span>Описание</span>
                                 <p>
@@ -53,20 +60,20 @@ $active_item = $house_cookie ? $house_cookie : 0;
         <div class="col-md-2">
             <h4 class="title">Характеристики</h4>
             <div id="characteristic_list">
-                <?= $this->render('data/characteristics_house', ['characteristics' => $houses_list[$house_cookie['key']]['characteristic']]) ?>
+                <?= $this->render('data/characteristics_house', ['characteristics' => $houses_list[$active_item['key']]['characteristic']]) ?>
             </div>
         </div>
         
         <div class="col-md-4">
             <div id="flats_list">
-                <?= $this->render('data/view_flats', ['flats' => $houses_list[$house_cookie['key']]['flat']]) ?>
+                <?= $this->render('data/view_flats', ['flats' => $houses_list[$active_item['key']]['flat']]) ?>
             </div>
         </div>
         
         <div class="col-md-2">
             <h4 class="title">Вложения</h4>
             <div id="files_list">
-                <?= $this->render('data/view_upload_files', ['files' => $houses_list[$house_cookie['key']]['image']]) ?>
+                <?= $this->render('data/view_upload_files', ['files' => $houses_list[$active_item['key']]['image']]) ?>
             </div>
         </div>
     </div>
