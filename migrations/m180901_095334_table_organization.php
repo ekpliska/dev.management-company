ce6cd2b88401b2cc706dc46dd8b3c26a473d8835
ekpliska <pliska.ek@gmail.com>
2019-03-01 11:53:44 +0300

    add: the deafult records in migration (organizations)

--- migrations/m180901_095334_table_organization.php
+++ migrations/m180901_095334_table_organization.php
@@ -24,7 +24,6 @@ class m180901_095334_table_organization extends Migration
             'email' => $this->string(150)->notNull(),
             'phone' => $this->string(150)->notNull(),
             'dispatcher_phone' => $this->string(150)->notNull(),
-            
             'postcode' => $this->integer()->notNull(),
             'town' => $this->string(100)->notNull(),
             'street' => $this->string(100)->notNull(),
@@ -38,6 +37,24 @@ class m180901_095334_table_organization extends Migration
         ], $table_options);
         
         $this->createIndex('idx-organizations-organizations_id', '{{%organizations}}', 'organizations_id');
+        
+        // Реквизиты компании
+        $this->batchInsert('{{%organizations}}', 
+                [
+                    'organizations_id', 'organizations_name', 'email', 'phone', 'dispatcher_phone',
+                    'postcode', 'town', 'street', 'house', 
+                    'time_to_work', 
+                    'inn', 'kpp',
+                    'checking_account', 'ks', 'bic'], 
+                [
+                    [
+                        '1', 'Управляющая компания', 'email@email.com', '+7 (000) 000-00-00', '8 000 000 00 00',
+                        '123456', 'Town', 'Street', 'House', 
+                        'Понедельник-Четверг: 00:00 - 00:00; Пятница: 00:00 - 00:00; Обед - 00:00 - 00:00', 
+                        '1234567890', '1234567890', 
+                        '1234567890', '1234567890', '1234567890'
+                    ]
+                ]);
 
     }
 
