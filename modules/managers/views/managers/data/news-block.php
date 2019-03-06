805b9b980f4a848a805a579b723ad585f197934f
ekpliska <pliska.ek@gmail.com>
2019-03-06 22:08:24 +0300

    fix: css media

--- modules/managers/views/managers/data/news-block.php
+++ modules/managers/views/managers/data/news-block.php
@@ -13,14 +13,13 @@
         <?php foreach ($news_content as $key => $post) : ?>
             <div class="__content-news">
                 <?= FormatHelpers::formatUrlNewsOrVote($post['news_title'], $post['slug']) ?>
-                <?php //  = Html::a($post['news_title'], ['/'], ['class' => 'title']) ?>
                 <p class="__content-date"><?= Yii::$app->formatter->asDate($post['created_at'], 'long') ?></p>
                 <div class="__content-news-item">
                     <div class="col-lg-4 col-md-12 col-sm-2 col-xs-6 __content-image">
                         <?= Html::img("/web/{$post['news_preview']}") ?>
                     </div>
                     <div class="col-lg-8 col-md-12 col-sm-10 col-xs-6 __content-text">
-                        <?= FormatHelpers::shortTextNews($post['news_text'], 15) ?>
+                        <?= FormatHelpers::shortTextNews($post['news_text'], 10) ?>
                     </div>                            
                 </div>
             </div>
