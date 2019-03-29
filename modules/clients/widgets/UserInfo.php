87f3c994a14e7b39c17950ae7f0a5ad96e1755ef
ekpliska <pliska.ek@gmail.com>
2019-03-30 00:09:31 +0300

    fix: user profile at navbar

--- modules/clients/widgets/UserInfo.php
+++ modules/clients/widgets/UserInfo.php
@@ -1,15 +1,25 @@
 <?php
 
     namespace app\modules\clients\widgets;
-    use yii\base\InvalidConfigException;
+    use Yii;
     use yii\base\Widget;
+    use app\models\Notifications;
 
 /**
  * Профиль пользователя из навигационного меню
  */
 class UserInfo extends Widget {
     
+    public $_notifications;
+    
+    
     public function init() {
+        
+        $this->_notifications = Notifications::find()
+                ->where(['user_uid' => Yii::$app->user->id])
+                ->orderBy(['created_at' => SORT_DESC])
+                ->limit(7)
+                ->all();
         
         parent::init();        
         
@@ -17,7 +27,9 @@ class UserInfo extends Widget {
 
     public function run() {
         
-        return $this->render('userinfo/profile');
+        return $this->render('userinfo/profile', [
+            'notifications_lists' => $this->_notifications,
+        ]);
         
     }
     
--- modules/clients/widgets/views/userinfo/profile.php
+++ modules/clients/widgets/views/userinfo/profile.php
@@ -11,7 +11,32 @@
 
 <div class="navbar_user-block">
     <div class="navbar_user-block__image">
-        <?= Html::img(Yii::$app->userProfile->photo, ['class' => 'user-profile__photo']) ?> 
+        <div class="dropdown">
+            <button class="dropdown-toggle" type="button" data-toggle="dropdown">
+                <?= Html::img(Yii::$app->userProfile->photo, ['class' => 'user-profile__photo']) ?>
+                <span class="<?= (count($notifications_lists) > 0) ? 'notification__dot' : 'hidden' ?>"></span>
+            </button>
+            <ul class="dropdown-menu notification__dropdown">
+                <?php if (!empty($notifications_lists) && count($notifications_lists) > 0) : ?>
+                <li class="text-right">
+                    <a href="javascript:void(0);" class="notification_reset">Отметить как прочитанные</a>
+                </li>
+                <li class="user-info-box">
+            
+                    <?php foreach ($notifications_lists as $note) : ?>
+                        <?php if ($note->type_notification == Notifications::TYPE_CHANGE_STATUS_IN_REQUEST) : ?>
+                            <?= Html::a("<span class='dot'></span>{$note->message}", ['requests/view-request', 'request_number' => $note->value_1], ['class' => 'notification_link', 'data-notice' => $note->id]) ?>
+                        <?php elseif ($note->type_notification == Notifications::TYPE_CHANGE_STATUS_IN_PAID_REQUEST) : ?>
+                            <?= Html::a("<span class='dot'></span>{$note->message}", ['paid-services/order-services'], ['class' => 'notification_link', 'data-notice' => $note->id]) ?>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+
+                </li>
+                <?php else: ?>
+                    <p>Новых уведомлений нет.</p>
+                <?php endif; ?>
+            </ul>
+        </div>
     </div>
     <div class="navbar_user-block__info">
         <p><?= Yii::$app->userProfile->getFullNameClient(false) ?></p>
