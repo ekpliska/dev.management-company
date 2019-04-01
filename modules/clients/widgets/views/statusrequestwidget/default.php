926f3299072edf6ec6ccad1b07d55f4b79d7de0a
ekpliska <pliska.ek@gmail.com>
2019-04-01 17:12:59 +0300

    fix: css

--- modules/clients/widgets/views/statusrequestwidget/default.php
+++ modules/clients/widgets/views/statusrequestwidget/default.php
@@ -6,20 +6,19 @@
 /* 
  * Вид статусов заявок
  */
-
 ?>
 <?php if (Yii::$app->controller->id == 'requests' && Yii::$app->controller->action->id == 'index') : ?>
-    <div class="navbar-request text-center">
-        <ul class="nav nav-pills status-request">
+<div class="status-request">
+    <ul class="nav nav-pills nav-justified status-request">
+        <li>
+            <?= Html::a('Все Заявки', ['requests/index'], ['class' => 'req-bange req-bange-default', 'data-status' => '-1']) ?>
+        </li>
+        <?php foreach ($status_requests as $key => $status) : ?>
             <li class="nav-item">
-                <?= Html::a('Все Заявки', ['requests/index'], ['class' => 'req-bange req-bange-default', 'data-status' => '-1']) ?>
+                <?= Html::a($status['name'], ['requests/index'], ['class' => $css_classes[$key] . ' status_request-switch', 'data-status' => $key]) ?>
+                <?= $key != StatusRequest::STATUS_CLOSE && $status['count'] != 0 ? '<span class="count-label">' . $status['count'] . '</span>' : '' ?>
             </li>
-            <?php foreach ($status_requests as $key => $status) : ?>
-                <li class="nav-item">
-                    <?= Html::a($status['name'], ['requests/index'], ['class' => $css_classes[$key] . ' status_request-switch', 'data-status' => $key]) ?>
-                    <?= $key != StatusRequest::STATUS_CLOSE && $status['count'] != 0 ? '<span class="count-label">' . $status['count'] . '</span>' : '' ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
+        <?php endforeach; ?>
+    </ul>
+</div>
 <?php endif; ?>
