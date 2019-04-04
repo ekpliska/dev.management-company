76849cd3ed7c8bdf272b7e1843854ad66258c0af
ekpliska <pliska.ek@gmail.com>
2019-04-04 12:54:35 +0300

    fix: NavBar css

--- modules/clients/widgets/views/navmenu/menu.php
+++ modules/clients/widgets/views/navmenu/menu.php
@@ -13,7 +13,7 @@ $controller = Yii::$app->controller->id;
 <ul class="menu-clients__navbar">
     <?php foreach ($menu_array as $key => $item) : ?>
         <li>
-            <a href="<?= Url::to([$item['link']]) ?>">
+            <a href="<?= Url::to([$item['link']]) ?>" class="<?= $controller == $key ? 'active-item-menu' : '' ?>">
                 <?= $item['name'] ?>
             </a>
         </li>
