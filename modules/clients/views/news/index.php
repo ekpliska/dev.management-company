aa2080b72ccef580042829adae0e07d10dd26e32
ekpliska <pliska.ek@gmail.com>
2019-04-05 14:45:58 +0300

    change: all news block

--- modules/clients/views/news/index.php
+++ modules/clients/views/news/index.php
@@ -1,6 +1,7 @@
 <?php
     
     use yii\helpers\Html;
+    use yii\helpers\Url;
     use yii\widgets\LinkPager;
     use app\modules\clients\widgets\SubBarNews;
     use app\helpers\FormatHelpers;
@@ -18,21 +19,23 @@ $this->title = Yii::$app->params['site-name'] . "Главная";
     <div class="row news-page__items">
     <?php foreach ($news as $key => $post) : ?>
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">
-            <div class="news-item">
-                <?= Html::img(Yii::getAlias('@web') . $post->news_preview, ['class' => 'news-item__preview']) ?>
-                <span class="news-item__rubric"><?= $post->rubric->rubrics_name ?></span>
-                <?php if (!empty($post->news_partner_id)) : ?>
-                    <?= Html::img(Yii::getAlias('@web') . $post->partner->partners_logo, ['class' => 'news__partner-logo', 'alt' => $post->partner->partners_name]) ?>
-                <?php endif; ?>
-                <div class="news-item__info">
-                    <h2 class="news-item__title">
-                        <?= FormatHelpers::shortTitleOrText($post->news_title, 65) ?>
-                    </h2>
-                    <p class="news-item__date">
-                        <?= FormatHelpers::formatDate($post->created_at, false) ?>
-                    </p>
+            <a href="<?= Url::to(['news/view', 'slug' => $post['slug']]) ?>">
+                <div class="news-item">
+                    <?= Html::img(Yii::getAlias('@web') . $post->news_preview, ['class' => 'news-item__preview']) ?>
+                    <span class="news-item__rubric"><?= $post->rubric->rubrics_name ?></span>
+                    <?php if (!empty($post->news_partner_id)) : ?>
+                        <?= Html::img(Yii::getAlias('@web') . $post->partner->partners_logo, ['class' => 'news__partner-logo', 'alt' => $post->partner->partners_name]) ?>
+                    <?php endif; ?>
+                    <div class="news-item__info">
+                        <h2 class="news-item__title">
+                            <?= FormatHelpers::shortTitleOrText($post->news_title, 65) ?>
+                        </h2>
+                        <p class="news-item__date">
+                            <?= FormatHelpers::formatDate($post->created_at, false) ?>
+                        </p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     <?php endforeach; ?>
     </div>
--- modules/clients/widgets/views/lastnews/default.php
+++ modules/clients/widgets/views/lastnews/default.php
@@ -1,8 +1,9 @@
 <?php
 
     use yii\widgets\LinkPager;
-    use app\helpers\FormatHelpers;
     use yii\helpers\Html;
+    use yii\helpers\Url;
+    use app\helpers\FormatHelpers;
 
 /* 
  * Новостная лента на главной
@@ -15,21 +16,23 @@
 </h1>
 <?php foreach ($news as $key => $post) : ?>
     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">
-        <div class="news-item">
-            <?= Html::img(Yii::getAlias('@web') . $post['news_preview'], ['class' => 'news-item__preview']) ?>
-            <span class="news-item__rubric"><?= $post['rubric']['rubrics_name'] ?></span>
-            <?php if (!empty($post['news_partner_id'])) : ?>
-                <?= Html::img(Yii::getAlias('@web') . $post['partner']['partners_logo'], ['class' => 'news__partner-logo', 'alt' => $post['partner']['partners_name']]) ?>
-            <?php endif; ?>
-            <div class="news-item__info">
-                <h2 class="news-item__title">
-                    <?= FormatHelpers::shortTitleOrText($post['news_title'], 65) ?>
-                </h2>
-                <p class="news-item__date">
-                    <?= FormatHelpers::formatDate($post['created_at'], false) ?>
-                </p>
+        <a href="<?= Url::to(['news/view', 'slug' => $post['slug']]) ?>">
+            <div class="news-item">
+                <?= Html::img(Yii::getAlias('@web') . $post['news_preview'], ['class' => 'news-item__preview']) ?>
+                <span class="news-item__rubric"><?= $post['rubric']['rubrics_name'] ?></span>
+                <?php if (!empty($post['news_partner_id'])) : ?>
+                    <?= Html::img(Yii::getAlias('@web') . $post['partner']['partners_logo'], ['class' => 'news__partner-logo', 'alt' => $post['partner']['partners_name']]) ?>
+                <?php endif; ?>
+                <div class="news-item__info">
+                    <h2 class="news-item__title">
+                        <?= FormatHelpers::shortTitleOrText($post['news_title'], 65) ?>
+                    </h2>
+                    <p class="news-item__date">
+                        <?= FormatHelpers::formatDate($post['created_at'], false) ?>
+                    </p>
+                </div>
             </div>
-        </div>
+        </a>
     </div>
 <?php endforeach; ?>
 <?php endif;?>
