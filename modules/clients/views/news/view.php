8b822126a3eff024fabd60e1d5e4f6f772611f88
ekpliska <pliska.ek@gmail.com>
2019-04-05 16:34:09 +0300

    change: the page of one of news

--- modules/clients/views/news/view.php
+++ modules/clients/views/news/view.php
@@ -1,39 +1,47 @@
 <?php
 
-    use app\helpers\FormatHelpers;
     use yii\helpers\Html;
-    use yii\helpers\Url;
-    use yii\widgets\Breadcrumbs;
+    use app\helpers\FormatHelpers;
 
 /* 
  * Просмотр отдельной новости
  */
 $this->title = Yii::$app->params['site-name'] .  $news['news_title'];
-$this->params['breadcrumbs'][] = ['label' => 'Новости', 'url' => ['clients/index']];
-$this->params['breadcrumbs'][] = $news['news_title'];
 ?>
 
-<?= Breadcrumbs::widget([
-        'homeLink' => ['label' => 'ELSA', 'url' => ['clients/index']],
-        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-]) ?>
-
-<div class="new-conteiner">
-    <div class="preview-news">
-    <?= FormatHelpers::previewNewsOrVote($news['news_preview'], true) ?>
-    <div class="news-rubric-block">
-        <span class="rubric-name"><?= $news['rubric']['rubrics_name'] ?></span>
-            <?php if ($news['isAdvert'] == 1) : ?>
-                <span class="partner-name"><?= $news['partner']['partners_name'] ?></span>
-            <?php endif; ?>
+<div class="news-conteiner">
+    <h2 class="news-conteiner__title">
+        <?= $news['news_title'] ?>
+    </h2>
+    
+    <ul class="list-inline post_details">
+        <li class="post_date">
+            <?= FormatHelpers::formatDate($news['created_at'], true, 1) ?>
+        </li>
+        <li class="post_rubric">
+            <?= $news['rubric']['rubrics_name'] ?>
+        </li>
+        <?php if (!empty($news['news_partner_id'])) : ?>
+        <li class="partners-info">
+            <span>От</span>&nbsp;
+            <?= Html::img(Yii::getAlias('@web') . $news['partner']['partners_logo'], [
+                    'class' => '', 
+                    'alt' => $news['partner']['partners_name']]) ?>
+            &nbsp;<span><?= $news['partner']['partners_name'] ?></span>
+        </li>
+        <?php endif; ?>
+        <li class="post_rubric">
+            <?= Html::a('Все новости', ['news/index']) ?>
+        </li>
+    </ul>
+    
+    <div class="news-conteiner__image">
+        <?= Html::img(Yii::getAlias('@web') . $news['news_preview'], [
+                'class' => 'news-prview', 
+                'alt' => $news['news_title']]); ?>
     </div>
-    <div class="news-title-block">
-        <h2 class="news-title"><?= $news['news_title'] ?></h2>
-        <p class="news-date"><?= FormatHelpers::formatDate($news['created_at'], false) ?></p>
-    </div>
-    </div>
-
-    <div class="news-text">
+    
+    <div class="news-conteiner__content">
         <?= $news['news_text'] ?>
     </div>
     
