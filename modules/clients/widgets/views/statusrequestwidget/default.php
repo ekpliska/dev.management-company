bcaa7d82c513e3a2831600834f821c0770a377f5
ekpliska <pliska.ek@gmail.com>
2019-04-05 17:00:57 +0300

    change: status reguests panel

--- modules/clients/widgets/views/statusrequestwidget/default.php
+++ modules/clients/widgets/views/statusrequestwidget/default.php
@@ -3,11 +3,28 @@
     use yii\helpers\Html;
     use app\models\StatusRequest;
 
-/* 
+/* <li><a href="#">Menu 1</a></li>
+        <li><a href="#">Menu 2</a></li>
+        <li><a href="#">Menu 3</a></li>
+      </ul>
  * Вид статусов заявок
  */
 ?>
-<?php if (Yii::$app->controller->id == 'requests' && Yii::$app->controller->action->id == 'index') : ?>
+
+<div class="status-request">
+    <ul class="nav nav-pills">
+        <li>
+            <?= Html::a('Все Заявки', ['requests/index'], ['class' => 'req-bange req-bange-default', 'data-status' => '-1']) ?>
+        </li>
+        <?php foreach ($status_requests as $key => $status) : ?>
+        <li>
+            <?= Html::a($status['name'], ['requests/index'], ['class' => $css_classes[$key] . ' status_request-switch', 'data-status' => $key]) ?>
+        </li>
+        <?php endforeach; ?>
+  </ul>
+</div>
+
+<?php /* if (Yii::$app->controller->id == 'requests' && Yii::$app->controller->action->id == 'index') : ?>
 <div class="status-request">
     <ul class="nav nav-pills nav-justified status-request">
         <li>
@@ -21,4 +38,4 @@
         <?php endforeach; ?>
     </ul>
 </div>
-<?php endif; ?>
+<?php endif; */ ?>
