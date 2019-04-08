c86d080bfbb00b11021692733862beb0d21afa47
ekpliska <pliska.ek@gmail.com>
2019-04-08 16:48:11 +0300

    create: the profile control block

--- modules/clients/views/profile/profile-control/profile-control.php
+++ modules/clients/views/profile/profile-control/profile-control.php
@@ -6,19 +6,36 @@
  * Управление профилем
  */
 ?>
-<div class="profile-control__nav">
-    <ul class="nav nav-pills">
-        <li>
-            <a href="#">Арендатор</a>
-        </li>
-        <li>
-            <a href="#">Платежи</a>
-        </li>
-        <li>
-            <a href="#">Приборы учета</a>
-        </li>
-    </ul>
-</div>
-<div class="profile-control">
-    
+
+<ul class="nav nav-tabs profile-control">
+    <li class="active">
+        <a data-toggle="tab" href="#rent">
+            Арендатор
+        </a>
+    </li>
+    <li>
+        <a data-toggle="tab" href="#paymants">
+            Платежи
+        </a>
+    </li>
+    <li>
+        <a data-toggle="tab" href="#counters">
+            Приборы учета
+        </a>
+    </li>
+</ul>
+
+<div class="tab-content profile-control">
+    <div id="rent" class="tab-pane fade in active">
+        <h3>Ареднатор</h3>
+        <?= $this->render('tabs/rent') ?>
+    </div>
+    <div id="paymants" class="tab-pane fade">
+        <h3>Платежи</h3>
+        <?= $this->render('tabs/payments') ?>
+    </div>
+    <div id="counters" class="tab-pane fade">
+        <h3>Приборы учета</h3>
+        <?= $this->render('tabs/counters') ?>
+    </div>
 </div>
---
+++ modules/clients/views/profile/profile-control/tabs/rent.php
@@ -0,0 +1,11 @@
+<?php
+
+    use yii\helpers\Html;
+
+/* 
+ * Вкладка "Арендатор"
+ */
+?>
+<div class="rent-tab">
+    <?= Html::button('Добавить арендатора') ?>
+</div>
