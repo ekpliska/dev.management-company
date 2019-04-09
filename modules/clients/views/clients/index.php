8a712493595682b7d285bd45595abd07294c08b3
ekpliska <pliska.ek@gmail.com>
2019-04-09 18:00:08 +0300

    fix: block at general page

--- modules/clients/views/clients/index.php
+++ modules/clients/views/clients/index.php
@@ -7,23 +7,33 @@
  * Главная страница личного кабинета Собственника
  */    
 $this->title = Yii::$app->params['site-name'] . "Новости";
+$current_date = date('F, Y');
 ?>
 
 <div class="client-page">
     <div class="row client-page__top">
         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
             <div class="__top-counters">
-                <h1>Month Year</h1>
+                <h1>
+                    <?= $current_date ?>
+                </h1>
+                <div class="__top-counters_content">
+                    #todo
+                </div>
             </div>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
             <div class="__top-payments">
-                <h1>Month Year</h1>
+                <h1>
+                    Платежи и квитанции
+                </h1>
             </div>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
             <div class="__top-promo">
-                <h1>Month Year</h1>
+                <h1>
+                    Промоблок
+                </h1>
             </div>
         </div>        
     </div>
