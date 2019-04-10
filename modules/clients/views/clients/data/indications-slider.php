772dddbc86abe16d3d7b8de301dba7dce97e307e
ekpliska <pliska.ek@gmail.com>
2019-04-10 15:50:28 +0300

    create: the slider to send indication at general page

--- modules/clients/views/clients/data/indications-slider.php
+++ modules/clients/views/clients/data/indications-slider.php
@@ -1,18 +1,54 @@
 <?php
 
     use yii\helpers\Html;
+    use app\models\TypeCounters;
 
 /* 
  * Слайдер для текущих показаний приборов учета
  */
+    
+$array_image = [
+    '1' => 'hold-water',
+    '2' => 'hot-water',
+    '3' => 'electric-meter',
+    '6' => 'electric-meter',
+    '4' => 'heating-meter',
+    '5' => 'heat-distributor',
+];
+$current_date = date('Y-m-d');
+$item = 0;
+$block_send = '<span class="block_send">Ввод показания заблокрован</span><br />' . Html::a('Мои приборы учета', ['counters/index']);
 ?>
-<?php // echo '<pre>'; var_dump($indications) ?>
 <?php if (!empty($indications) && is_array($indications)) : ?>
 <div class="counters-carousel owl-carousel owl-theme">
-    <?php foreach ($indications as $key => $indication) : ?>
-    <div class="item">
-        <?= Html::input('text', ['value' => $indication['Текущие показания']]) ?>
+    <?php for ($i = 0; $i < (count($indications))/2; $i++) : ?>
+    <div class="counters-item">
+        <div class="item">
+            <div>
+                <?php $data_check = (strtotime($current_date) >= strtotime($indications[$item]['Дата следующей поверки'])) ? false : true ?>
+                <?= Html::img('/images/counters/' . $array_image[TypeCounters::getTypeID($indications[$item]['Тип прибора учета'])] . '.svg', ['alt' => '', 'class' => 'counters-img']) ?>
+                <?= $indications[$item]['Тип прибора учета'] ?>
+            </div>
+            <div>
+                <?= $data_check ? Html::input('text', null, $indications[$item]['Текущее показание'], ['class' => 'slider-input']) : $block_send ?>
+            </div>
+        </div>
+        <?php $item++; ?>
+        <div class="item">
+            <div>
+                <?php $data_check = (strtotime($current_date) >= strtotime($indications[$item]['Дата следующей поверки'])) ? false : true ?>
+                <?= Html::img('/images/counters/' . $array_image[TypeCounters::getTypeID($indications[$item]['Тип прибора учета'])] . '.svg', ['alt' => '', 'class' => 'counters-img']) ?>
+                <?= $indications[$item]['Тип прибора учета'] ?>
+            </div>
+            <div>
+                <?= $data_check ? Html::input('text', null, $indications[$item]['Текущее показание'], ['class' => 'slider-input']) : $block_send ?>
+            </div>
+        </div>
+        <div class="item-btn">
+            <?= Html::button('Отправить', ['id' => "send-indication-{$i}"]) ?>
+        </div>
     </div>
-    <?php endforeach; ?>
+    <?php $item++; ?>
+    <?php endfor; ?>
 </div>
 <?php endif; ?>
--- modules/clients/widgets/views/lastnews/default.php
+++ modules/clients/widgets/views/lastnews/default.php
@@ -18,10 +18,10 @@
     <a href="<?= Url::to(['news/view', 'slug' => $post['slug']]) ?>">
         <div>
             <div class="news-item">
-                <?= Html::img(Yii::getAlias('@web') . '/web' . $post['news_preview'], ['class' => 'news-item__preview']) ?>
+                <?= Html::img(Yii::getAlias('@web') . $post['news_preview'], ['class' => 'news-item__preview']) ?>
                 <span class="news-item__rubric"><?= $post['rubric']['rubrics_name'] ?></span>
                 <?php if (!empty($post['news_partner_id'])) : ?>
-                    <?= Html::img(Yii::getAlias('@web') . '/web' . $post['partner']['partners_logo'], ['class' => 'news__partner-logo', 'alt' => $post['partner']['partners_name']]) ?>
+                    <?= Html::img(Yii::getAlias('@web') . $post['partner']['partners_logo'], ['class' => 'news__partner-logo', 'alt' => $post['partner']['partners_name']]) ?>
                 <?php endif; ?>
                 <div class="news-item__info">
                     <h2 class="news-item__title">
@@ -37,23 +37,3 @@
 <?php endforeach; ?>
 </div>
 <?php endif; ?>
-
-<?php /*
- * <a href="<?= Url::to(['news/view', 'slug' => $post['slug']]) ?>">
-            <div class="news-item">
-                <?= Html::img(Yii::getAlias('@web') . $post['news_preview'], ['class' => 'news-item__preview']) ?>
-                <span class="news-item__rubric"><?= $post['rubric']['rubrics_name'] ?></span>
-                <?php if (!empty($post['news_partner_id'])) : ?>
-                    <?= Html::img(Yii::getAlias('@web') . $post['partner']['partners_logo'], ['class' => 'news__partner-logo', 'alt' => $post['partner']['partners_name']]) ?>
-                <?php endif; ?>
-                <div class="news-item__info">
-                    <h2 class="news-item__title">
-                        <?= FormatHelpers::shortTitleOrText($post['news_title'], 65) ?>
-                    </h2>
-                    <p class="news-item__date">
-                        <?= FormatHelpers::formatDate($post['created_at'], false) ?>
-                    </p>
-                </div>
-            </div>
-        </a>
- */ ?>
