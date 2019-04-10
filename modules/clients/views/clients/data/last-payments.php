ae99af67655aad4592d6e7e73d8c70084440c549
ekpliska <pliska.ek@gmail.com>
2019-04-10 12:59:51 +0300

    fix: last payments

--- modules/clients/views/clients/data/last-payments.php
+++ modules/clients/views/clients/data/last-payments.php
@@ -8,28 +8,29 @@
 
 $count_payment = 6;
 $status_payment;
-//echo '<pre>'; var_dump($payments);
 ?>
 <?php if(!empty($payments) && is_array($payments)) : ?>
 <table class="table table-last-payment">
     <tbody>
         <tr>
             <?php foreach ($payments as $key => $payment) : ?>
-            <?php $status_payment = $payment['Статус квитанции'] == 'Не оплачена' ? false : true; ?>
-            <td>
-                <?= $status_payment ?
-                        $payment['Расчетный период'] :
-                        Html::a($payment['Расчетный период'], [
-                            'payments/payment', 
-                            'period' => urlencode($payment['Расчетный период']), 
-                            'nubmer' => urlencode($payment['Номер квитанции']), 
-                            'sum' => urlencode($payment['Сумма к оплате'])
-                        ]) 
-                ?>
-            </td>
-            <td class="<?= $status_payment ? 'status-payment-ok' : 'status-payment-not' ?>">
-                <?= $status_payment ? 'Оплачено' : 'Не оплачено' ?>
-            </td>
+            <?php if ($key <= 5) : ?>
+                <?php $status_payment = $payment['Статус квитанции'] == 'Не оплачена' ? false : true; ?>
+                <td>
+                    <?= $status_payment ?
+                            $payment['Расчетный период'] :
+                            Html::a($payment['Расчетный период'], [
+                                'payments/payment', 
+                                'period' => urlencode($payment['Расчетный период']), 
+                                'nubmer' => urlencode($payment['Номер квитанции']), 
+                                'sum' => urlencode($payment['Сумма к оплате'])
+                            ]) 
+                    ?>
+                </td>
+                <td class="<?= $status_payment ? 'status-payment-ok' : 'status-payment-not' ?>">
+                    <?= $status_payment ? 'Оплачено' : 'Не оплачено' ?>
+                </td>
+            <?php endif; ?>
             <?php endforeach; ?>
         </tr>
     </tbody>
