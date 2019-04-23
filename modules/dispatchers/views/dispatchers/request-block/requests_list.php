998b5593ebd527157de07cca341f10b4c8b53ec9
ekpliska <pliska.ek@gmail.com>
2019-04-23 21:18:00 +0300

    change: to group requests on status, dispaycher

--- modules/dispatchers/views/dispatchers/request-block/requests_list.php
+++ modules/dispatchers/views/dispatchers/request-block/requests_list.php
@@ -9,24 +9,27 @@
  */
 // На главной странице выводим все заявки первого пользователя по списку (список слева)
 $key = 0;
-$status_use = [];
 ?>
 
 <?php if (isset($user_lists) && !empty($user_lists)) : ?>
     <?php foreach ($user_lists[$key]['personalAccount'] as $key_account => $account) : // foreach 1 ?>
+    <?php $status_use = []; ?>
+        <p class="account-title">
+            <?= count($account['request']) > 0 ? "Лицевой счет {$account['account_number']}" : '' ?>
+        </p>
         <?php foreach ($account['request'] as $key_request => $request) : // foreach 2 ?>
+            <?php if (!in_array($request['status'], $status_use)) : ?>
             <div class="panel panel-default panel__request_block">
-                <?php if (!in_array($request['status'], $status_use)) : ?>
                 <?php $status_use[] = $request['status']; ?>
                 <div class="panel-heading panel-heading-<?= $request['status'] ?>">
                     <h4 class="panel-title">
-                        <a data-toggle="collapse" href="<?= "#status-{$request['status']}" ?>">
+                        <a data-toggle="collapse" href="<?= "#status-{$key_account}-{$request['status']}" ?>">
                             <?= StatusRequest::statusName($request['status']) ?> <i class="fa fa-sort-desc"></i>
                         </a>
                     </h4>
                 </div>
-                <?php endif; ?>
-                
+                <div id="<?= "status-{$key_account}-{$request['status']}" ?>" class="panel-collapse collapse">
+            <?php endif; ?>
                 <div class="dispatcher_genaral-page__request_block row">
                     <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12 request_block__info text-center">
                         <?= Html::a("ID{$request['requests_ident']}", [
@@ -50,47 +53,14 @@ $status_use = [];
                         </div>
                     </div>
                 </div>
-                
+                <?php if ($request['status'] != $account['request'][$key_request + 1]['status']) : ?>
+                </div>
             </div>
+            <?php endif; ?>
         <?php endforeach; // foreach 2 ?>
-        <?php endforeach; // foreach 1 ?>
+    <?php endforeach; // foreach 1 ?>
 <?php else : ?>
     <div class="notice info">
         <p>Список заявок пуст.</p>
     </div>
 <?php endif; ?>
-
-
-<?php /* if (isset($user_lists) && !empty($user_lists)) : ?>
-<?php foreach ($user_lists[$key]['personalAccount'] as $key_account => $account) : ?>
-    <?php foreach ($account['request'] as $key_request => $request) : ?>
-        <div class="dispatcher_genaral-page__request_block row">
-            <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12 request_block__info">
-                <?= StatusHelpers::requestStatusPage($request['status'], $request['created_at']) ?>
-                <span class="request_number">
-                    <?= "ID{$request['requests_ident']}" ?>
-                </span>
-                <?= Html::a('Перейти к заявке', ['requests/view-request', 'request_number' => $request['requests_ident']], ['class' => 'request__link-view']) ?>
-            </div>
-            <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12 request_block__adress text-right">
-                <?= FormatHelpers::formatFullAdress(
-                        $account['flat']['house']['houses_gis_adress'],
-                        $account['flat']['house']['houses_number'],
-                        false, false, 
-                        $account['flat']['flats_number']) ?>
-            </div>
-            <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12 request_block__content">
-                <?= FormatHelpers::shortTitleOrText($request['requests_comment'], 90) ?>
-                <div class="request_block__content_img">
-                    <?= FormatHelpers::imageRequestListByDispatcher($request['image']) ?>
-                </div>
-            </div>
-        </div>
-    <?php endforeach; ?>
-<?php endforeach; ?>
-<?php else : ?>
-    <div class="notice info">
-        <p>Список заявок пуст.</p>
-    </div>
-<?php endif; */ ?>
-        
