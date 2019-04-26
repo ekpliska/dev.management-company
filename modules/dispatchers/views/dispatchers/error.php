ca14f8fcf8aca29c2fd1c415499e9a34ebcbd927
ekpliska <pliska.ek@gmail.com>
2019-04-26 17:40:34 +0300

    create: to error page, dipatcher office

--- modules/dispatchers/views/dispatchers/error.php
+++ modules/dispatchers/views/dispatchers/error.php
@@ -1,8 +1,15 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Обработка ошибок
+ * 404, 403
  */
 ?>
+
+<div class="dispatcher-main-error">
+    <h4>
+        <?= $message ?>
+    </h4>
+</div>
+
+
