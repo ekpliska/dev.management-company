f84a062305d1b362bba888c09cb40b49c467ec6a
ekpliska <pliska.ek@gmail.com>
2019-05-14 10:46:38 +0300

    fix: payment system

--- config/payment_system.php
+++ config/payment_system.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'app\components\paymentSystem\PaymentSystem',
-    'public_id' => 'pk_1f5d5ad761f44549ac761e0329e86',  // Public ID из личного кабинета
-    'api_secret' => '979331b40c62fb869c8a10b377bf42ed'  // API Secret из личного кабинета
+    'public_id' => env('PUBLIC_ID'),  // Public ID из личного кабинета
+    'api_secret' => env('API_SECRET')  // API Secret из личного кабинета
 ];
 
 ?>
